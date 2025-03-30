7deb7c01cd0d0133fb95e82a52ab67af14ae66ec
YangW796 <135819221+YangW796@users.noreply.github.com>
2025-03-30 00:42:04 +0000

    v7

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -40,14 +40,14 @@ fn main() {}
 #[cfg(test)]
 mod tests {
 
-    // #[test]
-    // fn test_success() {
-    //     let timestamp = std::time::SystemTime::now()
-    //         .duration_since(std::time::UNIX_EPOCH)
-    //         .unwrap()
-    //         .as_secs();
-    //     let s = std::env::var("TEST_FOO").unwrap();
-    //     let e: u64 = s.parse().unwrap();
-    //    // assert!(timestamp >= e && timestamp < e + 10);
-    // }
+    #[test]
+    fn test_success() {
+        let timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let s = std::env::var("TEST_FOO").unwrap();
+        let e: u64 = s.parse().unwrap();
+       assert!(timestamp >= e && timestamp < e + 10);
+    }
 }
